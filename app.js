8fb225a3803cf7f18fbe4997e565abe536018bef
Patrick Pachur <patrick.pachur@gmail.com>
2017-11-24 13:20:18 +0100

    add gzip compression middleware

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -24,6 +25,7 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, limit: '4MB' }));
 app.use(cookieParser());
