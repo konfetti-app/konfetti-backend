04d7a77bb06776e235c49af29ebc3384d541b99c
Patrick Pachur <patrick.pachur@gmail.com>
2017-11-25 19:10:57 +0100

    include userId in JWT

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const issueToken = function (username) {
+const issueToken = function (user) {
   return new Promise((resolve, reject) => {
     const token = jwt.sign({
-      username: username,  // jwt configuration
+      username: user.username,  // jwt configuration
+      userId: user._id,
       iat: Math.floor(Date.now() / 1000) - 30 // date back 30 seconds
     },
     process.env.JWT_SECRET || 'shhhhh',
     {
-      expiresIn: '1d', // expire in 1 day
+      // expiresIn: '1d', // expire in 1 day
     });
     resolve(token);
   });
-}
+};
 
 exports.issueToken = issueToken;
