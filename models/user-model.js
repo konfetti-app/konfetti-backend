f6457393022e7f1d1c841b8224ea461f6649d302
Patrick Pachur <patrick.pachur@gmail.com>
2017-11-25 18:34:22 +0100

    populate avatar in getUser method /api/users/:id

--- models/user-model.js
+++ models/user-model.js
@@ -161,7 +161,7 @@ UserSchema.statics.getSingleUserFullyPopulated = function (username, callback) {
 
 UserSchema.statics.getSingleUser = function (userId, callback) {
   const User = mongoose.model('User');
-  User.findOne({_id: userId}).populate('neighbourhoods')
+  User.findOne({_id: userId}).populate('neighbourhoods avatar')
   .then((user) => {
     return new Promise(async (resolve, reject) => {
       function userHasRole(element) {
