e5cb732f991031bce7de7690b595c45cf50f11d4
Patrick Pachur <patrick.pachur@gmail.com>
2017-11-25 15:11:47 +0100

    towards chats 2

--- helpers/chats.js
+++ helpers/chats.js
@@ -5,11 +5,6 @@ const ChatChannel = mongoose.model('ChatChannel');
 const mubsub = require('mubsub');
 const mubsubClient = mubsub(`mongodb://${process.env.RUNS_IN_DOCKER ? 'mongo' : 'localhost'}/konfetti-mubsub`);
 
-const mubsubChannel = mubsubClient.channel('chats');
-
-mubsubClient.on('error', console.error);
-mubsubChannel.on('error', console.error);
-
 function init(server) {
   const io = require('socket.io')(server);
   const socketioJwt = require('socketio-jwt')
@@ -25,24 +20,43 @@ function init(server) {
     socket.emit('connection established');
 
     socket.on('room selection', function(data){
-		console.log('Joining room: ', data.roomID);
+		
         socket.join(data.roomID);
-        // do shit and send.
-    });
+        console.log(`socket: ${socket.decoded_token.username} ${socket.id} joined room: ${ data.roomID}`);
 
-    socket.on('subscribe', function(channelId) {
-        console.log(`subscribing ${socket.decoded_token.username} to channel ${channelId}`)
-        mubsubChannel.subscribe(channelId, function (message) {
-            console.log(`subscribed ${socket.decoded_token.username} to channel ${channelId}`);
-            
-          });
-          socket.on('chat message', function(msg){
-            console.log(`new message from io: ${socket.decoded_token.username} ${socket.id} ${msg}`);
-            mubsubChannel.publish(channelId, msg);
-          });
-    })
+        const mubsubChannel = mubsubClient.channel(data.roomID);
+        
+        mubsubClient.on('error', console.error);
+        mubsubChannel.on('error', console.error);
+
+        //DEBUG
+        setInterval(() => {
+          let date = Date()
+          console.log(`inserting new pubsub message: into room ${ data.roomID}: ${date}`);
+          mubsubChannel.publish('chat message', date);
+        }, 10000); // every 10 sec
+        // do shit and send.
+
+        mubsubChannel.subscribe('chat message', (msg) => {
+          console.log(`socket emitting to user: ${socket.decoded_token.username} socket: ${socket.id} room: ${ data.roomID}`)
+          socket.to(data.roomID).emit('chat message', msg);
+        })
     
 
+    // socket.on('subscribe', function(channelId) {
+    //     console.log(`subscribing ${socket.decoded_token.username} to channel ${channelId}`)
+    //     mubsubChannel.subscribe(channelId, function (message) {
+    //         console.log(`subscribed ${socket.decoded_token.username} to channel ${channelId}`);
+            
+    //       });
+    //       socket.on('chat message', function(msg){
+    //         console.log(`new message from io: ${socket.decoded_token.username} ${socket.id} ${msg}`);
+    //         mubsubChannel.publish(channelId, msg);
+    //       });
+    // });
+  });
+    
+    
     
 
     socket.on('disconnect', function() {
