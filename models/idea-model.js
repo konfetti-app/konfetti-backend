57940f28a51a04bd2916db55ba751bbcfadcf85c
Patrick Pachur <patrick.pachur@gmail.com>
2018-02-20 13:32:37 +0100

    create Idea - fixes #17

---
+++ models/idea-model.js
@@ -0,0 +1,258 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+const IdeaSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        default: 'idea'
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String,
+    },
+    address: {
+        type: String
+    },
+    geoData: { // differs from template to be closer to Neighbourhood
+        longitude: {
+            type: Number
+        },
+        latitude: {
+            type: Number
+        },
+        radius: {
+            type: Number
+        }
+    },
+    date: {
+        type: Date
+    },
+    wantsHelper: {
+        type: Boolean
+    },
+    helpDescription: {
+        type: String
+    },
+    wantsGuest: {
+        type: Boolean
+    },
+    reviewStatus: {
+        type: String // allowed: 'WAIT', 'OK', 'REJECTED'
+    },
+    konfettiTotal: {
+        type: Number
+    },
+    // konfettiUser // ? field is unclear.
+    context: {
+        type: String,
+        index: true
+    },
+    parentNeighbourhood: { // reference to parentNeighbourhood
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Neighbourhood'  
+    },
+    helpers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'  
+    }],
+    guests: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'  
+    }],
+    subscribers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'  
+    }],
+    // konfettiSpent: [{ // TODO: not implemented yet
+    //     byUser: { // Array of members in this channel
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'User'  
+    //     },
+    //     amount: {
+    //         type: Number
+    //     }
+    // }],
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        date: {
+            type: Number,
+            default: moment(new Date).unix()
+        },
+        byUser: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+});
+
+
+IdeaSchema.statics.createIdea = function (data, user, callback) {
+    const Idea = mongoose.model('Idea');
+    console.log(JSON.stringify(data));
+    let now = moment(new Date).unix();
+    let newIdea= new Idea({
+        title : data.title,
+        parentNeighbourhood: data.parentNeighbourhood,
+        geoData: data.geoData,
+        date: data.date,
+        address: data.address,
+        wantsHelper: data.wantsHelper,
+        wantsGuest: data.wantsGuest,
+        description: data.description,
+        helpDescription: data.helpDescription,
+        created : {
+            date: now,
+            byUser: user._id
+        }
+    }).save((err, doc) => {
+        if (err) {
+        console.log('Error saving new chat: ' + err.message);
+        callback(err, undefined);
+        } else {
+        callback(undefined, doc);
+        }
+    });
+};
+
+IdeaSchema.statics.getIdeas = function (params, callback) {
+
+    // TODO: remove Messages from results
+
+    const Idea = mongoose.model('Idea');
+    Idea.find({context: params.context, parentNeighbourhood: params.parentNeighbourhood, disabled: false}).populate({path: 'created.byUser', select: 'nickname avatar', populate: {path: 'avatar', select: 'filename'}}).exec(function (err, res) {
+        if (err) console.log(err);
+        callback(err, res);
+    });
+};
+
+IdeaSchema.statics.getChatById = function (chatId, callback) {
+    const Idea = mongoose.model('Idea');
+    Idea.findOne({_id: chatId}).exec(function (err, res) {
+        if (err) console.log(err);
+        callback(err, res);
+    });
+};
+
+// IdeaSchema.statics.createIdea = function (channelName, userId, callback) {
+//     const Idea = mongoose.model('Idea');
+//     Idea.findOne({name: channelName})
+//     .then((channel) => {
+//         if (!channel) {
+//             console.log('channelName not found. creating new channelName: ' + channelName);
+//             let now = moment(new Date).unix();
+//             let newChat = new Idea({
+//                 name : channelName,
+//                 created : {
+//                     date: now,
+//                     byUser: userId
+//                 }
+//             }).save((err, doc) => {
+//               if (err) {
+//                 console.log('Error saving new chat: ' + err.message);
+//                 callback(err, undefined);
+//               } else {
+//                 callback(undefined, doc);
+//               }
+//             });
+//     } else { // channel is already present
+//         callback(undefined, callback);
+//     }
+//   });
+// };
+
+IdeaSchema.statics.getChatMessagesSince = function (channel, since, user, callback) {
+    const ChatMessage = mongoose.model('ChatMessage');
+    const Idea = mongoose.model('Idea');
+    ChatMessage.find({parentChannel: channel, date : { $gt: since }}).limit(500).sort('date').populate({path: 'parentUser', select: 'nickname avatar', populate: {path: 'avatar', select: 'filename'}}).then(chatMessages => {
+        Idea.findOne({_id: channel}).select('subscribers').then(result =>{
+            // console.log('result:', JSON.stringify(result));
+            callback(null, chatMessages, result.subscribers.indexOf(user._id) > -1 ? true : false);
+        });
+    });
+};
+
+IdeaSchema.statics.deleteIdea = function (channel, user, callback) {
+    const Idea = mongoose.model('Idea');
+    Idea.findOne({_id: channel}).populate({path: 'created.byUser'}).then(idea => {
+        if (idea.created.byUser.equals(user) || user.isAdmin) {
+            idea.disabled = true;
+            idea.save((err, doc) => {
+                if (err) {
+                    console.log('Error deleting idea: ' + err.message);
+                    callback(err, undefined);
+                } else {
+                    console.log(`deleted idea ${doc._id} on behalf of user ${user._id}`);
+                    callback(undefined, doc);
+                }
+            });
+        } else {
+            callback('You are not allowed to delete this Channel', undefined);
+        }
+    });
+};
+
+// IdeaSchema.statics.createIdea = function (data, user, callback) {
+//     const Idea = mongoose.model('Idea');
+
+//     let now = moment(new Date).unix();
+//     let newChat = new Idea({
+//         // name : data.name,
+//         // parentNeighbourhood: data.parentNeighbourhood,
+//         // context: data.context,
+//         description: data.description,
+//         created : {
+//             date: now,
+//             byUser: user._id
+//         }
+//     }).save((err, doc) => {
+//         if (err) {
+//         console.log('Error saving new chat: ' + err.message);
+//         callback(err, undefined);
+//         } else {
+//         callback(undefined, doc);
+//         }
+//     });
+// };
+
+IdeaSchema.statics.subscribe = function (body, user, callback) { // requires body.ideaId
+    const Idea = mongoose.model('Idea');
+    if (body.id && user._id) {
+        Idea.findOneAndUpdate({_id: body.id}, {$addToSet:{subscribers: user._id}}, {upsert: true, new: true}, (err, subscriptions) => {
+            if (err) {
+                console.log(err);
+                callback(err, undefined);
+            } else {
+                console.log(`subscribed user ${user._id} to idea ${body.id}`);
+                callback(err, 'subscribed');
+            }
+        });
+    } else {
+        callback('invalid input', undefined);
+    }
+};
+
+IdeaSchema.statics.unsubscribe = function (someId, user, callback) { // requires body.ideaId
+    const Idea = mongoose.model('Idea');
+    if (someId && user._id) {
+        Idea.findOneAndUpdate({_id: someId}, {$pull:{subscribers: user._id}}, {upsert: true, new: true},
+            (err, subscriptions) => {
+                if (err) {
+                    console.log(err);
+                    callback(err, undefined);
+                } else {
+                    console.log(`unsubscribed ${user._id} from channel ${someId}`);
+                    callback(undefined, 'unsubscribed');
+                }
+            });
+    } else {
+        callback('invalid input', undefined);
+    }
+};
+
+
+mongoose.model('Idea', IdeaSchema);
---
+++ routes/api/ideas.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const formatError = require('../../helpers/errors.js').formatError;
+
+const mongoose = require('mongoose');
+const Idea = mongoose.model('Idea');
+
+/*
+    api routes
+
+    authenticate via basic auth against /api/authenticate and receive a JWT
+
+    afterwards, call endpoints with JWT in Header ("Authorization" : "Bearer <token>")
+    Also see supplied postman collection in /stuff/ for how to construct the Authorization Header
+*/
+
+/* POST create a new idea. */
+router.post('/', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+  console.log('pling')
+  Idea.createIdea(req.body, req.user, (err, idea) => {
+    if (err) res.status(500).json({code: 500, status: 'error', errors: [{err}]});
+    else res.status(201).json({code: 201, status: 'success', data: {idea: idea}});
+  });
+});
+
+// router.delete('/channel/:chatChannelId', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//   ChatChannel.deleteChatChannel(req.params.chatChannelId, req.user, (err, chatChannel) => {
+//     if (err) res.status(500).json({code: 500, status: 'error', errors: [{err}]});
+//     else res.status(200).json({code: 200, status: 'success', data: {chatChannel: chatChannel}});
+//   });
+// });
+
+// /* GET chatChannels for current user. */
+// router.get('/:parentNeighbourhood/:context', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//     // console.log(req.body);
+//     ChatChannel.getChatChannels(req.params, (err, chatChannels) => {
+//       if (err) res.status(500).json({code: 500, status: 'error', errors: [{err}]});
+//       else res.status(200).json({code: 200, status: 'success', data: {chatChannels: chatChannels}});
+//     });
+// });
+
+// /* GET chatMessages since timestamp for channel. */
+// router.get('/channel/:chatChannelId/since/:since', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//   // if no timestamp -> get all. result is array of ChatMessages.
+//   // console.log(req.body);
+//   ChatChannel.getChatMessagesSince(req.params.chatChannelId, req.params.since, req.user, (err, chatMessages, subscribed) => {
+//     if (err) res.status(500).json({code: 500, status: 'error', errors: [{err}]});
+//     else res.status(200).json({code: 200, status: 'success', data: {chatMessages: chatMessages, subscribed: subscribed}});
+//   });
+// });
+
+// /* POST subscribe to channel. */
+// router.post('/subscriptions', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//   console.log(req.body);
+//   // if (req.user.isAdmin) {
+//       ChatChannel.subscribe(req.body, req.user, (err, subscription) => {
+//           if (err) res.status(500).json({code: 500, status: 'error', errors: [formatError(err)]});
+//           else res.status(200).json({code: 200, status: 'ok', data: {subscription}});
+//       });
+//   // } else {
+//   //     res.status(403).json({code: 403, status: 'error', errors: ['not allowed to subscribe to this channel']});
+//   // }
+// });
+// /* DELETE unsubscribe from channel */
+// router.delete('/subscriptions/:id', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//       ChatChannel.unsubscribe(req.params.id, req.user, (err, subscription) => {
+//           if (err) res.status(500).json({code: 500, status: 'error', errors: [formatError(err)]});
+//           else res.status(200).json({code: 200, status: 'ok', data: {subscription}});
+//       });
+// });
+
+
+module.exports = router;
