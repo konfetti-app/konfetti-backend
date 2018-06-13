f2d3fe71f744380cb0ae7bed2ebc684c7183c319
Patrick Pachur <patrick.pachur@gmail.com>
2018-06-13 16:17:02 +0200

    towards account recovery

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const mongoose = require('mongoose');
 const jwt = require('../../helpers/jwt');
 const formatError = require('../../helpers/errors.js').formatError;
 
@@ -14,15 +15,35 @@ const formatError = require('../../helpers/errors.js').formatError;
 */
 
 router.post('/', passport.authenticate('basic', { session: false }), function(req, res, next) {
-    // console.log(req.body);
-    jwt.issueToken(req.user)
-    .then(token => {
-      res.status(200).json({code: 200, status: 'success', data: {token: token}});
-    })
-    .catch(err => {
-      console.log(`auth error: ${req.body.username}`);
-      res.status(500).json({code: 500, status: 'error', errors: [{err}]});
-    });
+  // console.log(req.body);
+  jwt.issueToken(req.user)
+  .then(token => {
+    res.status(200).json({code: 200, status: 'success', data: {token: token}});
+  })
+  .catch(err => {
+    console.log(`auth error: ${req.body.username}`);
+    res.status(500).json({code: 500, status: 'error', errors: [{err}]});
   });
+});
+
+router.post('/email', function(req, res, next) {
+  // console.log(req.body);
+  const User = mongoose.model('User');
+  User.findByEmailPassword(req.body.email, req.body.password, (err, user) => {
+
+    // TODO: implement.
+
+    jwt.issueToken(user)
+  .then(token => {
+    res.status(200).json({code: 200, status: 'success', data: {token: token}});
+  })
+  .catch(err => {
+    console.log(`auth error via email/pw: ${JSON.stringify(err)}`);
+    res.status(500).json({code: 500, status: 'error', errors: [{err}]});
+  });
+
+  });
+  
+});
 
 module.exports = router;
